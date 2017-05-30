b2549253a60f99b5b18072493f1bb893a5061e38
Your Name <you@example.com>
2017-05-30 20:03:36 +0900

    com

--- test_same.sh
+++ test_same.sh
@@ -1,7 +1,7 @@
 #!/bin/ sh
-if [ $1 = 2 ]; then if [ $1 = 2 ]; then
-echo "$1 $2 OK" echo "$1 $2 OK"
+if [ $1 = 2 ]; then
+echo "$1 $2 OK"
 else
-echo "$1 $2 NG" echo "$1 $2 NG"
+echo "$1 $2 NG"
 fi
 
