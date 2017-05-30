8a518a3d0afdf9cd427239ea7ddaedfc76122209
Your Name <you@example.com>
2017-05-30 20:58:59 +0900

    kadai3

--- test_euclid.sh
+++ test_euclid.sh
@@ -8,7 +8,7 @@ ERROR_EXIT(){
  exit 1
 }
 
-echo "same2.sh requires 2 string inputs" > $tmp-errmes1
+echo "The number of arguments is not two." > $tmp-errmes1
 echo "same2.sh requires 2 string inputs" > $tmp-errmes2
 echo "same2.sh requires 2 string inputs" > $tmp-errmes3
 ./euclid.sh 20> $tmp-err1 && ERROR_EXIT "TEST2-1"
