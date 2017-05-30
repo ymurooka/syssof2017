322dbc35eaa112d807b964cb951185ba19b1c10e
Your Name <you@example.com>
2017-05-30 21:04:41 +0900

    kadai3

--- test_euclid.sh
+++ test_euclid.sh
@@ -11,9 +11,9 @@ ERROR_EXIT(){
 echo "The number of arguments is not two." > $tmp-errmes1
 echo "The first argument is not a natural number." > $tmp-errmes2
 echo "The second argument is not a natural number." > $tmp-errmes3
-./euclid.sh 20> $tmp-err1 && ERROR_EXIT "TEST1-1"
-./euclid.sh 2.1 2> $tmp-err2 && ERROR_EXIT "TEST2-1"
-./euclid.sh 2 2.1> $tmp-err3 && ERROR_EXIT "TEST3-1"
+./euclid.sh 20 > $tmp-err1 && ERROR_EXIT "TEST1-1"
+./euclid.sh 2.1 2 > $tmp-err2 && ERROR_EXIT "TEST2-1"
+./euclid.sh 2 2.1 > $tmp-err3 && ERROR_EXIT "TEST3-1"
 diff $tmp-errmes1 $tmp-err1 || ERROR_EXIT "TEST1-2"
 diff $tmp-errmes2 $tmp-err2 || ERROR_EXIT "TEST2-2"
 diff $tmp-errmes3 $tmp-err3 || ERROR_EXIT "TEST3-2"
